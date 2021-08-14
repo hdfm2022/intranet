596cda5013f58dc43ca1823c7e517140ccf485e4
hdfm2022 <hdfmacedo+git@gmail.com>
2021-08-14 00:52:08 -0300

    configurando database sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,21 @@
+import "reflect-metadata";
 import express from "express";
 
-// @types/express (precisa instalar separado, pra ter as tipagens)
+import "./database"; // vai pro index
+
 const app = express();
 
-/** 
- * GET          => Buscar informações
- * POST         => Inserir (criar) uma informação
- * PUT          => Alterar uma informação
- * DELETE       => Deletar um dado
- * PATCH        => Alterar uma informação específica (somente a senha do usuário, não todo ele)
-*/
+// app.get("/test", (request, response) => {
+//     return response.send("Teste GET")
+// })
 
- /**
-  * Tipos de parametros
-  *  Routes Params => http://localhost:3000/produtos/3/ 
-  *  Querys Params => http://localhost:3000/produtos?name=teclado&max=100000
-  *  Body Params   => {
-  *     "name": "teclado"
-  *    , "description" : "teclado rgb"
-  * }
-  * Usado para post/put/patch. Nunca get/delete.
-  */
+// app.post('/test-post', (request, response) => {
+//     return response.send("Teste POST")
+// })
 
-app.get("/test", (request, response) => {
-    return response.send("Teste GET")
-})
-
-app.post('/test-post', (request, response) => {
-    return response.send("Teste POST")
-})
-
-// http://localhost:3000
-app.listen(3000, () => console.log("Server is running Intranet"));
+const httpPort = 3000;
+app.listen(httpPort, () => {
+    console.clear()
+    console.log("Commbox Intranet Server is running")
+    console.log("HTTP Port "+httpPort+"")
+});
